9daf071e50acf4d6389434952bace4cc23938c13
romansharapov19 <r.sharapov@kigroup.de>
2022-03-25 00:00:06 +0000

    first fix

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -1,10 +1,7 @@
 import Image from "next/image"
-import Link from "next/image"
-import {router} from "next/client";
 
 function SmallCard({img, location, distance}) {
     return (
-        <button >
             <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition-transform duration-200 ease-out">
                 {/* Left */}
                 <div className="relative h-16 w-16">
@@ -22,8 +19,6 @@ function SmallCard({img, location, distance}) {
                     <h3 className="text-gray-500">{distance}</h3>
                 </div>
             </div>
-        </button>
-
     )
 }
 
