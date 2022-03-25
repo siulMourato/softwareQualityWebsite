b139a45e791f972c69e8b0f2b557fc30f4001267
romansharapov19 <r.sharapov@kigroup.de>
2022-03-25 00:14:47 +0000

    creat londo page and redirect to there from london click smallCard

--- components/SmallCard.js
+++ components/SmallCard.js
@@ -1,8 +1,16 @@
 import Image from "next/image"
+import { useRouter } from 'next/router'
 
 function SmallCard({img, location, distance}) {
+    const router = useRouter()
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        router.push(`/${location.toLowerCase()}`)
+    }
+
     return (
-            <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition-transform duration-200 ease-out">
+            <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition-transform duration-200 ease-out" onClick={handleClick}>
                 {/* Left */}
                 <div className="relative h-16 w-16">
 
