30f6fa143b06dadf460cb953d16f3cd9b32e10f2
romansharapov19 <r.sharapov@kigroup.de>
2022-05-05 21:15:49 +0100

    test 2

--- pages/london.js
+++ pages/london.js
@@ -14,7 +14,7 @@ export async function getServerSideProps() {
     },
   };
 }
-//TODO: just a test
+//TODO: just a test 2
 function London({ searchResults }) {
   return (
     <div className="h-screen">
