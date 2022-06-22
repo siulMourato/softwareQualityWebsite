8781df2c67d5224261b8bbd2b6e3a4e1e64d8aff
romansharapov19 <r.sharapov@kigroup.de>
2022-06-22 22:07:19 +0100

    add test to house details page

--- cypress/integration/houseDetailsPage.feature
+++ cypress/integration/houseDetailsPage.feature
@@ -2,25 +2,26 @@ Feature: Show house details page
   As a user
   I want to select a house form presented list
   So that I can see the house details in a single page with all the information
-  
+
   Scenario Outline: Show the selected house details
     Given I can open the application
     And access to the landing page
-    And search for houses for today
+    And search '<Search>' for houses for today
     When I see a list off houses
     And  I select '<House>' and click
     Then I can see right page '<House>' opened
+    And  I can see that the URL contains '<Search>'
     And  I can see short description '<ShortDescriptionType>'
     And  I can see detailed description '<DetailedDescription>'
     And  I can see Price is '<Priceperday>'
     And  I can see button "Reserve"
 
 
-Examples: 
-  | House                                  | ShortDescriptionType                   | DetailedDescription                                                                                  | Priceperday  |
-  | London_Studio_Apartments               | London Studio Apartments               | 4 guest · 4 bedroom · 4 bed · 2 bathrooms · Free parking · Washing Machine                           | 35             |
-  | Independant_luxury_studio_apartment    | Independant luxury studio apartment    | 2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen                                   | 40             |
-  | Spacious_Peaceful_Modern_Bedroom       | Spacious Peaceful Modern Bedroom       | 3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Free parking · Dry Cleaning               | 60             |
+    Examples:
+      | Search  | House                               | ShortDescriptionType                | DetailedDescription                                                                    | Priceperday |
+      | London  | London_Studio_Apartments            | London Studio Apartments            | 4 guest · 4 bedroom · 4 bed · 2 bathrooms · Free parking · Washing Machine             | 35          |
+      | Leiria  | Independant_luxury_studio_apartment | Independant luxury studio apartment | 2 guest · 3 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen                     | 40          |
+      | Abudabi | Spacious_Peaceful_Modern_Bedroom    | Spacious Peaceful Modern Bedroom    | 3 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Free parking · Dry Cleaning | 60          |
 
 #validar retorna de 7 items
 #validar que têm o preço
--- cypress/integration/houseDetailsPage/houseDetailsPage.js
+++ cypress/integration/houseDetailsPage/houseDetailsPage.js
@@ -1,43 +1,48 @@
 Given('I can open the application', () => {
-    cy.visit('http://35.192.209.229/')
+  cy.visit('http://35.192.209.229/');
 });
 
 And('access to the landing page', () => {
-    cy.get('.pl-5').type("London")
-    cy.log('First page')
-    cy.get('.pt-6 > .text-4xl').should('have.text', 'Explore Nearby')
+  cy.get('.pt-6 > .text-4xl').should('have.text', 'Explore Nearby');
 });
-And('search for houses for today', () => {
-    cy.get('.w-12').clear().type("0")
-    cy.get('.rdrStaticRangeSelected > .rdrStaticRangeLabel').should('have.text', 'Today')
-    cy.get(':nth-child(3) > .text-red-400').click({force: true})
+And('search {string} for houses for today', (Search) => {
+  cy.get('.pl-5').type(Search);
+  cy.get('.rdrStaticRangeSelected > .rdrStaticRangeLabel').should(
+    'have.text',
+    'Today',
+  );
+  cy.get(':nth-child(3) > .text-red-400').click({ force: true });
 });
 When('I see a list off houses', () => {
-    cy.get('.py-7').should('have.length', 7)
-    cy.get('.py-7 > .flex-col > .items-end > div > .text-lg').should('have.length', 7)
-    cy.get('.py-7 > .relative > span > .rounded-2xl').should('have.attr', 'src')
+  cy.get('.py-7').should('have.length', 7);
+  cy.get('.py-7 > .flex-col > .items-end > div > .text-lg').should(
+    'have.length',
+    7,
+  );
+  cy.get('.py-7 > .relative > span > .rounded-2xl').should('have.attr', 'src');
 });
 
 And('I select {string} and click', (House) => {
-    cy.get('#' + House).click({force: true})
-
-  
+  cy.get('#' + House).click({ force: true });
 });
 Then('I can see right page {string} opened', (House) => {
-
-    /* MISSING TEST */
+  cy.get('.text-xl').should('have.text', House.replaceAll('_', ' '));
+});
+And('I can see that the URL contains {string}', (Search) => {
+  cy.location().should((location) => {
+    expect(location.pathname).contains(Search);
+  });
 });
 And('I can see short description {string}', (ShortDescriptionType) => {
-    cy.get('.text-xl').should('have.text', ShortDescriptionType);
+  cy.get('.text-xl').should('have.text', ShortDescriptionType);
 });
 And('I can see detailed description {string}', (DetailedDescription) => {
-    cy.get('.flex-col > .text-sm').should('have.text', DetailedDescription);
+  cy.get('.flex-col > .text-sm').should('have.text', DetailedDescription);
 });
 And('I can see Price is {string}', (Priceperday) => {
-    cy.get('.text-lg').should('contains.text', Priceperday);
+  cy.get('.text-lg').should('contains.text', Priceperday);
 });
 
 And('I can see button "Reserve"', () => {
-    cy.get('.p-3').should('have.text', 'Reserve now')
+  cy.get('.p-3').should('have.text', 'Reserve now');
 });
-
